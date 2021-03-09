53c420aa3062eedd9f506fe9b9363aff23b32371
Michael Levitin <michael9levi@gmail.com>
2021-03-09 23:19:32 +0300

    Completed 1st draft realisation/Override

--- src/ru/javawebinar/webapp/storage/ArrayStorage.java
+++ src/ru/javawebinar/webapp/storage/ArrayStorage.java
@@ -1,35 +1,58 @@
 package ru.javawebinar.webapp.storage;
 
 import ru.javawebinar.webapp.model.Resume;
+
+import java.util.Arrays;
 import java.util.Collection;
 
 public class ArrayStorage implements IStorrage {
 
-  private  Resume[] array = new Resume[100];
+  private static final int LIMIT = 100;
+  private Resume[] array = new Resume[LIMIT];
 
   @Override
   public void clear() {
-
+    Arrays.fill(array, null);
   }
 
   @Override
   public void save(Resume r) {
-
+      for (int i = 0; i < LIMIT; i++) {
+        Resume resume = array[i];
+        if(resume == null)
+          array[i] = r;
+        else if (r.equals(resume))
+          throw new IllegalStateException("Already present");
+      }
   }
 
   @Override
   public void update(Resume r) {
-
+    for (int i = 0; i < array.length; i++) {
+      if (array[i].getUuid().equals(r.getUuid())) {
+        array[i] = r;
+        return;
+      }
+    }
   }
 
   @Override
   public Resume load(String uuid) {
+    for (Resume resume : array) {
+      if (resume.getUuid().equals(uuid))
+        return resume;
+    }
     return null;
   }
 
   @Override
   public void delete(String uuid) {
-
+    for (int i = 0; i < array.length; i++) {
+      if (array[i].getUuid().equals(uuid)) {
+        array[i] = null;
+        return;
+      }
+    }
   }
 
   @Override
@@ -39,6 +62,12 @@ public class ArrayStorage implements IStorrage {
 
   @Override
   public int size() {
-    return 0;
+    int arraySize = 0;
+    for (Resume resume : array) {
+      if (resume.getUuid() == null)
+        break;
+      arraySize++;
+    }
+    return arraySize;
   }
 }
