fc27178686cdc5d7c7598230824e8e9b6d368484
Michael Levitin <michael9levi@gmail.com>
2021-03-10 21:03:54 +0300

    Added getIndex, RW all methods

--- src/ru/javawebinar/webapp/storage/ArrayStorage.java
+++ src/ru/javawebinar/webapp/storage/ArrayStorage.java
@@ -1,14 +1,18 @@
 package ru.javawebinar.webapp.storage;
 
+import ru.javawebinar.webapp.WebAppException;
 import ru.javawebinar.webapp.model.Resume;
-
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.logging.Logger;
 
 public class ArrayStorage implements IStorrage {
 
   private static final int LIMIT = 100;
   private Resume[] array = new Resume[LIMIT];
+//  protected Logger LOGGER = Logger.getLogger(getClass().getName());
+  private static Logger LOGGER = Logger.getLogger(ArrayStorage.class.getName());
+  private int size = 0;
 
   @Override
   public void clear() {
@@ -17,47 +21,53 @@ public class ArrayStorage implements IStorrage {
 
   @Override
   public void save(Resume r) {
-      for (int i = 0; i < LIMIT; i++) {
-        Resume resume = array[i];
-        if(resume == null)
-          array[i] = r;
-        else if (r.equals(resume))
-          throw new IllegalStateException("Already present");
-      }
+    LOGGER.info("Saving resume with Uiid" + r.getUuid());
+    int idx =  getIndex(r.getUuid());
+    if (idx != -1)
+//      try {
+//        throw new WebAppException("Resume already exists");
+//      } catch (WebAppException e) {
+//        LOGGER.log(Level.SEVERE, e.getMessage(), e);
+//        throw new IllegalStateException(e); // обертывание exception
+//      }
+        throw new WebAppException("Resume" + r.getUuid() + "already exists");
+      array[size++] = r;
   }
 
   @Override
   public void update(Resume r) {
-    for (int i = 0; i < array.length; i++) {
-      if (array[i].getUuid().equals(r.getUuid())) {
-        array[i] = r;
-        return;
-      }
-    }
+    LOGGER.info("Updated resume with uiid = " + r.getUuid());
+    int idx = getIndex(r.getUuid());
+    if (idx == -1)
+      throw new WebAppException("Resume with uiid = " + r.getUuid() + "doesn't exists");
+    array[idx] = r;
   }
 
   @Override
   public Resume load(String uuid) {
-    for (Resume resume : array) {
-      if (resume.getUuid().equals(uuid))
-        return resume;
-    }
-    return null;
+    LOGGER.info("Loading resume with uiid = " + uuid);
+    int idx = getIndex(uuid);
+    if (idx == -1)
+      throw new WebAppException("Resume with uiid = " + uuid + "doesn't exists");
+    return array[idx];
   }
 
   @Override
   public void delete(String uuid) {
-    for (int i = 0; i < array.length; i++) {
-      if (array[i].getUuid().equals(uuid)) {
-        array[i] = null;
-        return;
-      }
-    }
+    LOGGER.info("Deleting resume with uiid = " + uuid);
+    int idx = getIndex(uuid);
+    if (idx == -1)
+       throw new WebAppException("Resume with uiid = " + uuid + "doesn't exists");
+    int numMoved = size - idx - 1;
+    if (numMoved > 0)
+      System.arraycopy(array, idx+1, array, idx, numMoved);
+    array[--size] = null; // Clear to let GC to do it's magic
   }
 
   @Override
   public Collection<Resume> getAllSorted() {
-    return null;
+    Arrays.sort(array, 0, size);
+    return Arrays.asList(Arrays.copyOf(array, size));
   }
 
   @Override
@@ -69,5 +79,14 @@ public class ArrayStorage implements IStorrage {
       arraySize++;
     }
     return arraySize;
+  }
+
+  protected int getIndex(String uuid) {
+      for (int i = 0; i < array.length; i++) {
+        if (array[i] != null)
+          if (array[i].getUuid().equals(uuid))
+            return i;
+      }
+      return -1;
   }
 }
