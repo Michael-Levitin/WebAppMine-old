0609767cb5791152fa40e7a0b165050f5cbdf7c2
Michael Levitin <michael9levi@gmail.com>
2021-03-13 23:58:58 +0300

    reusing Main

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,11 @@
-import ru.javawebinar.webapp.model.Link;
+import ru.javawebinar.webapp.model.Contact;
+import ru.javawebinar.webapp.model.ContactType;
+import ru.javawebinar.webapp.model.Resume;
 
-import java.lang.reflect.Field;
 
 public class Main {
-  public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
-
-    int i = 5;
-    Link l1 = new Link("JavaWebinar", "javawebinar.ru");
-    Link l2 = l1;
-    Link l3 = new Link(l1);
-    System.out.println(l1.getClass());
-    System.out.println(l1.equals(l3));
-    System.out.println(l1);
-
-    System.out.println(Link.EMPTY);
-    System.out.println(Link.empty());
-    Field f = Link.class.getDeclaredField("url");
-    f.setAccessible(true);
-    Link l = new Link("fghfh", "URL");
-    System.out.println(l instanceof Link);
-    System.out.println(Link.class.isInstance(l));
-    System.out.println(f.get(l));
-    System.out.println(l.getUrl());
+  public static void main(String[] args) {
+    Resume R1 = new Resume("Полное имя1", "location1");
+    R1.addContact(new Contact(ContactType.MAIL, "mail@yandex.ru"));
   }
 }
